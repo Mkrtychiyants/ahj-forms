5f57e2783528abba07c1e1638832ccbd41dc03d0
Mkrtychiyants <heisaloosershesad@gmail.com>
2023-09-12 17:24:46 +1000

    fourth commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
-// TODO: write code here
-
 const { default: PopupWidget } = require('./component/popuoWidget');
 const { default: Popup } = require('./component/popup');
 
 const button = document.querySelector('button');
 
-PopupWidget(button, new Popup());
+const test = new PopupWidget(button, new Popup());
+const Console = window.console;
+Console.log(test);
